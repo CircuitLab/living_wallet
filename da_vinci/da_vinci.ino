4cd1d30ccdaa1e164441b7eb65484f4f397a257e
hideyukisaito <hideyuki@uniba.jp>
2013-08-06 17:56:56 +0900

    update stability

--- da_vinci/da_vinci.ino
+++ da_vinci/da_vinci.ino
@@ -1,172 +1,259 @@
 #include <MsTimer2.h>
 
+// Macros for Pins of Da Vinci 32u4
+#define PB0 17 // BIN_REAR_1
+#define PB1 15 // BIN_REAR_2
+#define PB2 16 // STBY_FRONT
+#define PB3 14 // BIN_FRONT_1
+#define PB4 8  // AIN_FRONT_1
+#define PB5 9  // AIN_FRONT_2
+#define PB6 10 // PWM_REAR_B
+#define PB7 11 // PWM_FRONT_A
+#define PC6 5  // PWM_FRONT_B
+#define PC7 13 // KONASHI_PIN_4
+#define PD0 3
+#define PD1 2  // AIN_REAR_2
+#define PD2 0  // RX
+#define PD3 1  // TX
+#define PD4 4  // AIN_REAR_1
+#define PD5    // (TXLED)
+#define PD6 12 // BIN_FRONT_2
+#define PD7 6  // PWM_REAR_A
+#define PE2    // (HWB)
+#define PE6 7  // KONASHI_PIN_5
+#define PF0 23 // (A5) FLEX_SENSOR_PIN
+#define PF1 22 // (A4)
+#define PF4 21 // (A3) IR_SENSOR_PIN
+#define PF5 20 // (A2)
+#define PF6 19 // (A1) STBY_REAR
+#define PF7 18 // (A0) KONASHI_PIN_3
+
+
 #define STATE_NEUTRAL 0
-#define STATE_DISABLE_CONSUME 1
-#define STATE_ENABLE_CONSUME 2
+#define STATE_ENABLE_CONSUME 1
+#define STATE_DISABLE_CONSUME 2
 
 // MsTimer2でD3とD11を使用
 
-#define STBY_FRONT 16 // PB6 : standby
-#define STBY_REAR  19  // PF5
+// Front Motor Stand-by
+#define FRONT_STBY PF6
 
 // Front Motor A
-#define PWM_FRONT_A 10 //Speed control 
-#define AIN_FRONT_1 14 //Direction
-#define AIN_FRONT_2 8 //Direction
+#define FRONT_PWM_A PD7 //OK
+#define FRONT_AIN_1 PD4 //OK
+#define FRONT_AIN_2 PD1 //OK
 
 // Front Motor B
-#define PWM_FRONT_B 5 //Speed control
-#define BIN_FRONT_1 15 //Direction
-#define BIN_FRONT_2 12 //Direction
+#define FRONT_PWM_B PB6 //OK
+#define FRONT_BIN_1 PB0 //OK
+#define FRONT_BIN_2 PB1 //OK
+
+
+// Rear Motor Stand-by
+#define REAR_STBY PB2
 
 // Rear Motor A
-#define PWM_REAR_A 6
-#define AIN_REAR_1 2
-#define AIN_REAR_2 4
+#define REAR_PWM_A PB7 //Speed control 
+#define REAR_AIN_1 PB4 //Direction
+#define REAR_AIN_2 PB5 //Direction
 
 // Rear Motor B
-#define PWM_REAR_B 9
-#define BIN_REAR_1 17
-#define BIN_REAR_2 18
+#define REAR_PWM_B PC6 //Speed control
+#define REAR_BIN_1 PB3 //Direction
+#define REAR_BIN_2 PD6 //Direction
 
-#define IR_SENSOR_PIN A4
+#define IR_SENSOR_PIN PF4
+#define IR_SENSOR_THRESHOLD 600
 //#define IR_LED_PIN 13
-volatile int irValue = 0;
+volatile int irSensorValue = 0;
 
-#define FLEX_SENSOR_PIN A5
+#define FLEX_SENSOR_PIN PF0
+#define FLEX_SENSOR_THRESHOLD 700
 volatile int flexSensorValue = 0;
 
-#define STATE_PIN 21
-volatile int state = LOW;
+//#define STATE_PIN PF4
+//volatile int state = LOW;
 
-#define KONASHI_PIN_4 13
-#define KONASHI_PIN_5 7
+#define KONASHI_PIN_3 PF7
+#define KONASHI_PIN_4 PC7
+#define KONASHI_PIN_5 PE6
+//#define KONASHI_PIN_5_INTERRUPT 4
 volatile int konashiPin4PrevValue = LOW;
 volatile int konashiPin5PrevValue = LOW;
 volatile int konashiPin4Value = LOW;
 volatile int konashiPin5Value = LOW;
 
-boolean isConsumable = false;
+#define FRONT_WHEEL_RIGHT 0
+#define FRONT_WHEEL_LEFT 1
+#define REAR_WHEEL_RIGHT 0
+#define REAR_WHEEL_LEFT 1
+#define DIRECTION_FORWARD 1
+#define DIRECTION_BACK 0
+
+
+volatile int prevState;
 volatile int currentState;
+
+boolean bAvoided = false;
+boolean enableAvoidance = false;
+
+
+// Arduinoとのコミュニケーション用
+#define START_BYTE 0x7E
+#define END_BYTE 0x88
+#define COMMAND_NEUTRAL 0x00
+#define COMMAND_ENABLE_CONSUME 0x10
+#define COMMAND_DISABLE_CONSUME 0x20
+#define COMMAND_CAUGHT 0x30
 
 void setup()
 {
   Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial1.begin(57600);
   
   analogReference(INTERNAL);
   
   // setup front motors
-  pinMode(STBY_FRONT, OUTPUT);
+  pinMode(FRONT_STBY, OUTPUT);
 
-  pinMode(PWM_FRONT_A, OUTPUT);
-  pinMode(AIN_FRONT_1, OUTPUT);
-  pinMode(AIN_FRONT_2, OUTPUT);
+  pinMode(FRONT_PWM_A, OUTPUT);
+  pinMode(FRONT_AIN_1, OUTPUT);
+  pinMode(FRONT_AIN_2, OUTPUT);
 
-  pinMode(PWM_FRONT_B, OUTPUT);
-  pinMode(BIN_FRONT_1, OUTPUT);
-  pinMode(BIN_FRONT_2, OUTPUT);
+  pinMode(FRONT_PWM_B, OUTPUT);
+  pinMode(FRONT_BIN_1, OUTPUT);
+  pinMode(FRONT_BIN_2, OUTPUT);
   
   // setup rear motors
-  pinMode(STBY_REAR, OUTPUT);
+  pinMode(REAR_STBY, OUTPUT);
   
-  pinMode(PWM_REAR_A, OUTPUT);
-  pinMode(AIN_REAR_1, OUTPUT);
-  pinMode(AIN_REAR_2, OUTPUT);
+  pinMode(REAR_PWM_A, OUTPUT);
+  pinMode(REAR_AIN_1, OUTPUT);
+  pinMode(REAR_AIN_2, OUTPUT);
   
-  pinMode(PWM_REAR_B, OUTPUT);
-  pinMode(BIN_REAR_1, OUTPUT);
-  pinMode(BIN_REAR_2, OUTPUT);
+  pinMode(REAR_PWM_B, OUTPUT);
+  pinMode(REAR_BIN_1, OUTPUT);
+  pinMode(REAR_BIN_2, OUTPUT);
   
-  pinMode(KONASHI_PIN_4, INPUT);
+  pinMode(KONASHI_PIN_3, OUTPUT);
   pinMode(KONASHI_PIN_5, INPUT);
-    
+  
+  digitalWrite(KONASHI_PIN_3, LOW);
   konashiPin4PrevValue = digitalRead(KONASHI_PIN_4);
   konashiPin5PrevValue = digitalRead(KONASHI_PIN_5);
   
-  currentState = STATE_NEUTRAL;
+  // KONASHIの4ピンがLOWなら消費可能、HIGHなら不可能
+  if (konashiPin4PrevValue == konashiPin5PrevValue) {
+    currentState = STATE_NEUTRAL;
+  } else if (HIGH == konashiPin4PrevValue) {
+    currentState = STATE_ENABLE_CONSUME;
+  } else if (HIGH == konashiPin5PrevValue) {
+    currentState = STATE_DISABLE_CONSUME;
+  } 
   
-  MsTimer2::set(200, observeInputs);
+  prevState = currentState;
+  
+//  attachInterrupt(KONASHI_PIN_5_INTERRUPT, changeStateByKonashi, CHANGE);
+  
+  MsTimer2::set(100, observeInputs);
   MsTimer2::start();
 }
 
 void loop()
 {
-  if (STATE_ENABLE_CONSUME == currentState) {
-    moveFront(1, 255, 1);
-    moveFront(1, 255, 1);
-    delay(300);
-    stopFront();
-    
-    moveRear(1, 255, 1);
-    moveRear(2, 255, 1);
-    delay(300);
-    stopRear();
-  } else if (STATE_DISABLE_CONSUME == currentState) {
-    
+  String serialMsg = "";
+  while (Serial1.available()) {
+    serialMsg += Serial1.read();
+//    Serial.println(Serial1.read());
   }
   
-//  moveFront(1, 255, 1); //motor 1, full speed, left
-//  moveFront(2, 255, 1); //motor 2, full speed, left
-//
-//  delay(200); //go for 1 second
-//  stopFront(); //stop
-//  delay(250); //hold for 250ms until moveFront again
-//
-//  moveFront(1, 255, 0); //motor 1, half speed, right
-//  moveFront(2, 255, 0); //motor 2, half speed, right
-//
-//  delay(200);
-//  stopFront();
-//  delay(250);
-//  
-//  moveRear(1, 255, 1);
-//  moveRear(2, 255, 1);
-//  
-//  delay(200);
-//  stopRear();
-//  delay(250);
-//  
-//  moveRear(1, 255, 0); //motor 1, half speed, right
-//  moveRear(2, 255, 0); //motor 2, half speed, right
-//  
-//  delay(200);
-//  stopRear();
-//  delay(250);
+  if (0 != strcmp(serialMsg.c_str(), "")) {
+    Serial.print("serial input: ");
+    Serial.println(serialMsg);
+  }
+  
+  if (STATE_NEUTRAL == currentState) {
+   moveFront(FRONT_WHEEL_RIGHT, 255, DIRECTION_FORWARD);
+    delay(10);
+    moveFront(FRONT_WHEEL_LEFT, 255, DIRECTION_FORWARD);
+    delay(500);
+    stopFront();
+//    delay(100);
+    
+    moveRear(REAR_WHEEL_RIGHT, 255, DIRECTION_FORWARD);
+    delay(10);;
+    moveRear(REAR_WHEEL_LEFT, 255, DIRECTION_FORWARD);
+    delay(500);
+    stopRear();
+  } else if (STATE_ENABLE_CONSUME == currentState) {
+    moveFront(FRONT_WHEEL_RIGHT, 255, DIRECTION_FORWARD);
+    delay(10);
+    moveFront(FRONT_WHEEL_LEFT, 255, DIRECTION_FORWARD);
+    delay(500);
+    stopFront();
+//    delay(100);
+    
+    moveRear(REAR_WHEEL_RIGHT, 255, DIRECTION_FORWARD);
+    delay(10);;
+    moveRear(REAR_WHEEL_LEFT, 255, DIRECTION_FORWARD);
+    delay(500);
+    stopRear();
+//    delay(100);
+  } else if (STATE_DISABLE_CONSUME == currentState) {
+    if (enableAvoidance) {
+      moveFront(FRONT_WHEEL_RIGHT, 255, DIRECTION_FORWARD);
+      delay(10);
+      moveFront(FRONT_WHEEL_LEFT, 100, DIRECTION_BACK);
+      delay(10);
+      stopFront();
+      
+      moveRear(REAR_WHEEL_RIGHT, 255, DIRECTION_FORWARD);
+      delay(10);
+      moveRear(REAR_WHEEL_LEFT, 100, DIRECTION_BACK);
+      delay(10);
+      stopRear();
+    } else {
+      moveFront(0, 255, 1);
+      moveFront(1, 255, 1);
+      delay(200);
+      stopFront();
+      
+      moveRear(0, 255, 0);
+      moveRear(1, 255, 0);
+      delay(200);
+      stopRear();
+    }
+  }
 }
 
-
+// 前輪を動かす
 void moveFront(int motor, int speed, int direction)
-{
-//move specific motor at speed and direction
-//motor: 0 for B 1 for A
-//speed: 0 is off, and 255 is full speed
-//direction: 0 clockwise, 1 counter-clockwise  
-  digitalWrite(STBY_FRONT, HIGH); //disable standby
-  
+{ 
+  digitalWrite(FRONT_STBY, HIGH); //disable standby
 
   boolean inPin1 = LOW;
   boolean inPin2 = HIGH;
 
-  if(1 == direction) {
+  if (1 == direction) {
     inPin1 = HIGH;
     inPin2 = LOW;
   }
 
-  if(1 == motor) {
-    digitalWrite(AIN_FRONT_1, inPin1);
-    digitalWrite(AIN_FRONT_2, inPin2);
-    analogWrite(PWM_FRONT_A, speed);
-  } else {
-    digitalWrite(BIN_FRONT_1, inPin1);
-    digitalWrite(BIN_FRONT_2, inPin2);
-    analogWrite(PWM_FRONT_B, speed);
+  if (0 == motor) {
+    digitalWrite(FRONT_AIN_1, inPin1);
+    digitalWrite(FRONT_AIN_2, inPin2);
+    analogWrite(FRONT_PWM_A, speed);
+  } else if (1 == motor) {
+    digitalWrite(FRONT_BIN_1, inPin1);
+    digitalWrite(FRONT_BIN_2, inPin2);
+    analogWrite(FRONT_PWM_B, speed);
   }
 }
 
+// 後輪を動かす
 void moveRear(int motor, int speed, int direction)
 {  
-  digitalWrite(STBY_REAR, HIGH);
+  digitalWrite(REAR_STBY, HIGH);
   
   boolean inPin1 = LOW;
   boolean inPin2 = HIGH;
@@ -176,82 +263,114 @@ void moveRear(int motor, int speed, int direction)
     inPin2 = LOW;
   }
   
-  if (1 == motor) {
-    digitalWrite(AIN_REAR_1, inPin1);
-    digitalWrite(AIN_REAR_2, inPin2);
-    analogWrite(PWM_REAR_A, speed);
-  } else {
-    digitalWrite(BIN_REAR_1, inPin1);
-    digitalWrite(BIN_REAR_2, inPin2);
-    analogWrite(PWM_REAR_B, speed);
+  if (0 == motor) {
+    digitalWrite(REAR_AIN_1, inPin1);
+    digitalWrite(REAR_AIN_2, inPin2);
+    analogWrite(REAR_PWM_A, speed);
+  } else if (1 == motor) {
+    digitalWrite(REAR_BIN_1, inPin1);
+    digitalWrite(REAR_BIN_2, inPin2);
+    analogWrite(REAR_PWM_B, speed);
   }
 }
 
+// 前輪をストップさせる
 void stopFront()
 {
 //enable standby  
-  digitalWrite(STBY_FRONT, LOW); 
+  digitalWrite(FRONT_STBY, LOW); 
 }
 
+// 後輪をストップさせる
 void stopRear()
 {
-  digitalWrite(STBY_REAR, LOW);
+  digitalWrite(REAR_STBY, LOW);
 }
 
+// 障害物を回避する
+void avoid()
+{
+  Serial.println("avoid");
+  bAvoided = true;
+}
+
+// Arduinoへコマンドを送信し、叫び声を再生する
+void playShoutSound()
+{
+  Serial.println("shout");
+}
+
+// Arduinoへコマンドを送信し、歓喜の歌を再生する
+
+// KONASHIの4ピンを監視する
+void changeStateByKonashi()
+{
+  currentState = HIGH == digitalRead(KONASHI_PIN_5) ? STATE_DISABLE_CONSUME : STATE_ENABLE_CONSUME;
+  
+  if (HIGH == currentState) {
+    Serial.println("consumable");
+  } else {
+    Serial.println("not consumable");
+    
+    bAvoided = false;
+  }
+}
+
+// 測距センサーと曲げセンサーの値を監視する
 void observeInputs()
 {
-  irValue = analogRead(IR_SENSOR_PIN);  
-  flexSensorValue = analogRead(FLEX_SENSOR_PIN);
+//  Serial.println("check analog values");
   
-  // konashiの4、5番ピンを監視する
   konashiPin4Value = digitalRead(KONASHI_PIN_4);
   konashiPin5Value = digitalRead(KONASHI_PIN_5);
+  irSensorValue = analogRead(IR_SENSOR_PIN);  
+  flexSensorValue = analogRead(FLEX_SENSOR_PIN);
+//  Serial.print("IR: ");
+//  Serial.print(irSensorValue);
+//  Serial.print(", FLEX: ");
+//  Serial.print(flexSensorValue);
+//  Serial.print(", STATE: ");
+//  Serial.println(currentState);
   
   if (LOW == konashiPin4PrevValue && HIGH == konashiPin4Value) {
-    Serial.println("konashi pin 4 is rising");
-    Serial1.println("pin 4 rising");
-    
     currentState = STATE_ENABLE_CONSUME;
-  } else if (HIGH == konashiPin4PrevValue && LOW == konashiPin4Value) {
-    Serial.println("konashi pin 4 is falling");
-    Serial1.println("pin 4 falling");
   }
   
   if (LOW == konashiPin5PrevValue && HIGH == konashiPin5Value) {
-    Serial.println("konashi pin 5 is rising");
-    Serial1.println("pin 5 rising");
-    
     currentState = STATE_DISABLE_CONSUME;
-  } else if (HIGH == konashiPin5PrevValue && LOW == konashiPin5Value) {
-    Serial.println("konashi pin 5 is falling");
-    Serial1.println("pin 5 falling");
   }
   
-  if (HIGH == konashiPin4Value && HIGH == konashiPin5Value) {
+  if (konashiPin4Value == konashiPin5Value) {
     currentState = STATE_NEUTRAL;
+  }
+  
+  if (prevState != currentState) {
+    Serial1.print((char)START_BYTE);
+    
+    if (STATE_ENABLE_CONSUME == currentState) {
+      Serial1.print((char)COMMAND_ENABLE_CONSUME);
+    } else if (STATE_DISABLE_CONSUME == currentState) {
+      Serial1.print((char)COMMAND_DISABLE_CONSUME);
+    } else {
+      // i.e. currentState = STATE_NEUTRAL
+      Serial1.print((char)COMMAND_NEUTRAL);
+    }
+    
+    Serial1.print((char)END_BYTE);
+    
+    prevState = currentState;
   }
   
   konashiPin4PrevValue = konashiPin4Value;
   konashiPin5PrevValue = konashiPin5Value;
   
-  float realVoltageIR = irValue / 1024.0 * 5.0;
-  float realVoltageFlex = flexSensorValue / 1024.0 * 5.0;
-//  Serial.print("IR Sensor: ");
-//  Serial.print(realVoltageIR);
-//  Serial.print(", Flex Sensor: ");
-//  Serial.print(realVoltageFlex);
-//  Serial.print(", KONASHI pin 4: ");
-//  Serial.print(konashiPin4Value);
-//  Serial.print(", KONASHI pin 5: ");
-//  Serial.println(konashiPin5Value);
-}
-
-void serialEvent()
-{
-  Serial.println("serial event");
-  while (Serial1.available()) {
-    char inChar = (char)Serial1.read();
-    Serial.print("serial input: ");
-    Serial.println(inChar);
+  if (STATE_DISABLE_CONSUME == currentState && IR_SENSOR_THRESHOLD <= irSensorValue) {
+    enableAvoidance = true;
+  } else {
+    enableAvoidance = false;
+  }
+  
+  if (STATE_DISABLE_CONSUME == currentState && FLEX_SENSOR_THRESHOLD <= flexSensorValue) {
+    playShoutSound();
   }
 }
--- pro_mini/pro_mini.ino
+++ pro_mini/pro_mini.ino
@@ -1,31 +1,67 @@
+#include <SPI.h>
+#include <SdFat.h>
+#include <SdFatUtil.h>
+#include <SFEMP3Shield.h>
+
+SdFat sd;
+SFEMP3Shield MP3player;
+
 int debugPin = 13;
 
-int delayTime = 1000;
+int delayTime = 2000;
 
 int command;
 
+// Da Vinciとのコミュニケーション用
+#define START_BYTE 0x7E
+#define END_BYTE 0x88
+#define COMMAND_NEUTRAL 0x00
+#define COMMAND_ENABLE_CONSUME 0x10
+#define COMMAND_DISABLE_CONSUME 0x20
+#define COMMAND_CAUGHT 0x30
+
+volatile byte prevCommand;
+volatile byte currentCommand;
+
+String trackNamesOnEnableConsume[] = { "track001" };
+String trackNamesOnDisableConsume[] = { "track003", "track004" };
+
+volatile byte state;
+
 void setup()
 {
-  Serial.begin(9600);  
+  Serial.begin(57600);  
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+  
+  if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) sd.initErrorHalt();
+  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
+  
+  uint8_t result = MP3player.begin();
+  if (0 != result) {
+    Serial.print(F("Error code: "));
+    Serial.print(result);
+    Serial.println(F(" when trying to start MP3 player"));
+    
+    if(6 == result) {
+      Serial.println(F("Warning: patch file not found, skipping."));
+      Serial.println(F("Use the \"d\" command to verify SdCard can be read"));
+    }
+  }
+  
+  union twobyte mp3_vol;
+  mp3_vol.word = MP3player.getVolume();
+  mp3_vol.byte[1] = 2;
+  MP3player.setVolume(mp3_vol.byte[1], mp3_vol.byte[1]);
+  
+  randomSeed(analogRead(0));
+  
+  pinMode(debugPin, OUTPUT);
 }
 
 void loop()
 {
-  if (0 < Serial.available()) {
-    delayTime = 250;
-    
-    while (Serial.available()) {
-      Serial.println(Serial.read());
-    }
-//    command = Serial.read();
-//    Serial.println(command);
-  } else {
-    delayTime = 1000;
-  }
-  
   debugBlink();
 }
 
@@ -35,4 +71,64 @@ void debugBlink()
   delay(delayTime);
   digitalWrite(debugPin, LOW);
   delay(delayTime);
+}
+
+void serialEvent()
+{  
+  while (Serial.available()) {
+    if (START_BYTE == (char)Serial.read()) {
+      byte command =  (char)Serial.read();
+      
+      if (COMMAND_ENABLE_CONSUME == command) {
+        currentCommand = COMMAND_ENABLE_CONSUME;
+      } else if (COMMAND_DISABLE_CONSUME == command) {
+        currentCommand = COMMAND_DISABLE_CONSUME;
+      } else if (COMMAND_CAUGHT == command) {
+        currentCommand = COMMAND_CAUGHT;
+      } else {
+        currentCommand = COMMAND_NEUTRAL;
+      }
+      
+      if (prevCommand != currentCommand) {
+        uint8_t result;
+        
+        if (COMMAND_ENABLE_CONSUME == currentCommand) {
+          result = MP3player.playMP3("song_01.mp3", 0);
+        } else if (COMMAND_DISABLE_CONSUME == currentCommand) {
+          result = MP3player.playMP3("dont_touch_01.mp3", 0);
+        } else if (COMMAND_CAUGHT == currentCommand) {
+          result = MP3player.playMP3("help_01.mp3", 0);
+        } else {
+          MP3player.stopTrack();
+          result = 0;
+        }
+        
+        if (0 != result) {
+          Serial.print(F("Error code: "));
+          Serial.print(result);
+          Serial.println(F(" when trying to play track"));
+        }
+      }
+      
+      prevCommand = currentCommand;
+      
+      Serial.print((char)currentCommand);
+      Serial.flush();
+      break;
+    }
+  }
+//  if (1 < Serial.available()) {
+//    if (START_BYTE == Serial.read()) {
+//      if (COMMAND_ENABLE_CONSUME == Serial.read()) {
+//        delayTime = 250;
+//        Serial.println("enable consume");
+//      } else if (COMMAND_DISABLE_CONSUME == Serial.read()) {
+//        delayTime = 500;
+//        Serial.println("disable consume");
+//      } else if (COMMAND_CAUGHT == Serial.read()) {
+//        delayTime = 1000;`
+//        Serial.println("caught");
+//      }
+//    }
+//  }
 }
